c3346c9a0e72b7c32b6cbd375f3a776362f5e4d5
Maaxboon <maaxwelokoth@gmail.com>
2024-06-21 06:14:07 -0700

    fixed indentation to comply with PEP 8

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for alpha_letters in range(ord('a'), ord('z')+1):
     if chr(alpha_letters) == 'e' or chr(alpha_letters) == 'q':
-       continue
+        continue
     print("{:c}".format(alpha_letters), end="")
