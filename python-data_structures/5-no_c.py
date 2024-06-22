1bd482d575ecbcdfe04eb29a69f3fab80931e3a5
Maaxboon <maaxwelokoth@gmail.com>
2024-06-22 11:47:01 -0700

    removes all characters c and C from a string

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -7,4 +7,4 @@ def no_c(my_string):
     for i in my_string:
         if ord('i') == 99 or 67:
             my_string.translate({ord('c'): None, ord('C'): None})
-    return my_string.translate({ord('c'): None, ord('C'): None})r
+    return my_string.translate({ord('c'): None, ord('C'): None})
