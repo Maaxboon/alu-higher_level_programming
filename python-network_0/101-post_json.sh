f35daad9949da2683ea6f72901f7764d026540d2
Maaxboon <maaxwelokoth@gmail.com>
2024-07-22 04:43:12 -0700

    adding a script that sends a JSON POST request

--- python-network_0/101-post_json.sh
+++ python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends POST request with JSON file to given URL and displays response
-curl -sF "data=$2" "$1"
+# Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
