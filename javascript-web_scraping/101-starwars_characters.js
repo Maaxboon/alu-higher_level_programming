5095c9cba99706465db26b1e54079c6816749fec
Maaxboon <maaxwelokoth@gmail.com>
2024-09-30 09:50:39 +0300

    script that prints all characters of a Star Wars movie

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const axios = require('axios');
+const request = require('request');
 
 if (process.argv.length !== 3) {
   console.error('Usage: node 101-starwars_characters.js <Movie ID>');
@@ -9,24 +9,39 @@ if (process.argv.length !== 3) {
 const movieId = process.argv[2];
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-axios.get(apiUrl)
-  .then(response => {
-    const film = response.data;
-
-    if (!film || !film.characters || film.characters.length === 0) {
-      console.error('No characters found for the specified movie ID.');
-      process.exit(1);
-    }
-
-    const charactersUrls = film.characters;
-    const charactersPromises = charactersUrls.map(url => axios.get(url).then(res => res.data.name));
-
-    return Promise.all(charactersPromises);
-  })
-  .then(characters => {
-    characters.forEach(character => console.log(character));
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error.response ? error.response.status : error.message);
+request(apiUrl, (error, response, body) => {
+  if (error || response.statusCode !== 200) {
+    console.error('Error:', error || `Status Code: ${response.statusCode}`);
     process.exit(1);
+  }
+
+  const film = JSON.parse(body);
+
+  if (!film || !film.characters || film.characters.length === 0) {
+    console.error('No characters found for the specified movie ID.');
+    process.exit(1);
+  }
+
+  const charactersUrls = film.characters;
+  const charactersPromises = charactersUrls.map(url => {
+    return new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          reject(error || `Status Code: ${response.statusCode}`);
+        } else {
+          const character = JSON.parse(body);
+          resolve(character.name);
+        }
+      });
+    });
   });
+
+  Promise.all(charactersPromises)
+    .then(characters => {
+      characters.forEach(character => console.log(character));
+    })
+    .catch(error => {
+      console.error('Error fetching character:', error);
+      process.exit(1);
+    });
+}); // Make sure there is only one newline at the end of the file
